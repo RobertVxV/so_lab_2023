dea507509e4c2f6847bfb1dd2491e6f05bcea589
RobertVxV <robertvinitchi@gmail.com>
2023-10-13 22:43:25 +0300

    added writing to output_file

--- lab3/count.sh
+++ lab3/count.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# set_mode.sh <directory> <mode>
-# nerecursiv si recursiv
+# count.sh <directory> <output_file>
 
 if [ $# -ne 2 ]
 then
     echo "Error! Usage: <script_name> <directory> <output_file>."
 else
+        directory=$1
+        output_file=$2
 
-        if [ -d $1 ]
+        if [ -f $output_file ]
+        then rm $output_file
+        fi
+        
+        if [ -d $directory ]
         then 
-            cd $1
+            cd $directory
             total_chars=0
             for line in $(ls *.txt | cut -f 9 -d ' ') # <=> (ls *.txt | cat) // displays each file on a new line
             do
@@ -18,14 +23,15 @@ else
                 then 
                     for chars in $(wc -c $line | cut -f 1 -d ' ')
                     do
-                        echo "$line $chars"
+                        echo "$line $chars" >> $output_file
                         total_chars=`expr $total_chars + $chars`
                     done
                 else 
                     echo "Script failed for $line, not a regular file."
                 fi
             done
-            echo "TOTAL $total_chars"
+            echo "TOTAL $total_chars" >> $output_file
+            mv $output_file "../$output_file"
         else
             echo "Error: $1 is not a directory or doesn't exist."
             fi
