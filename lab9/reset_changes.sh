8a1cc4e4e30d3b01130052b36a516f6f5a52deaa
RobertVxV <robertvinitchi@gmail.com>
2023-12-09 07:34:39 -0500

    changed script to relative path

--- lab9/reset_changes.sh
+++ lab9/reset_changes.sh
@@ -1,7 +1,7 @@
-cp "/home/robert/github/so_lab_2023/lab9/imagini/imagine_1.bmp" "./testfolder_input/imagine_1.bmp"
-cp "/home/robert/github/so_lab_2023/lab9/imagini/imagine_2.bmp" "./testfolder_input/imagine_2.bmp"
-cp "/home/robert/github/so_lab_2023/lab9/imagini/imagine_3.bmp" "./testfolder_input/imagine_3.bmp"
-cp "/home/robert/github/so_lab_2023/lab9/imagini/imagine_4.bmp" "./testfolder_input/imagine_4.bmp"
-cp "/home/robert/github/so_lab_2023/lab9/imagine.bmp" "./testfolder_input/imagine_frumoasa.bmp"
+cp "./imagini/imagine_1.bmp" "./testfolder_input/imagine_1.bmp"
+cp "./imagini/imagine_2.bmp" "./testfolder_input/imagine_2.bmp"
+cp "./imagini/imagine_3.bmp" "./testfolder_input/imagine_3.bmp"
+cp "./imagini/imagine_4.bmp" "./testfolder_input/imagine_4.bmp"
+cp "./imagine.bmp" "./testfolder_input/imagine_frumoasa.bmp"
 rm -r ./testfolder_output/*
 rm -r ./testfolder_output/.*
